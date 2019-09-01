4189c82c8b10d024e2c71bff753b68bb03e16242
akshay sharma <32414883+Akshaysharma1999@users.noreply.github.com>
2019-09-01 12:54:41 +0530

    deploy

--- src/actions/index.js
+++ src/actions/index.js
@@ -118,7 +118,7 @@ export const getFeedArticles = ()=>{
 export const getMyArticles = (username)=>{
     return async (dispatch,getState)=>{
         const response = await realworld.get('/articles',{
-        headers:`Token ${getState.profile.user.token}`,
+        headers:headers,
             params:{
                 author:username
             }
